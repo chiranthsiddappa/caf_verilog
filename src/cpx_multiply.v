ee3052d8dc209527d897c7577c50d34f584bc04b
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2018-10-28 20:07:34 -0600

    New changes for pipelining and axi

--- src/cpx_multiply.v
+++ src/cpx_multiply.v
@@ -8,13 +8,23 @@ module cpx_multiply #(parameter xi_bits = 12,
 		      parameter q_bits = 24
 		      )
    (input clk,
-    input signed [xi_bits-1:0] 	  xi,
-    input signed [xq_bits-1:0] 	  xq,
-    input signed [yi_bits-1:0] 	  yi,
-    input signed [yq_bits-1:0] 	  yq,
+    input                          m_axis_x_tvalid,
+    input signed [xi_bits-1:0]     xi,
+    input signed [xq_bits-1:0]     xq,
+    input                          m_axis_y_tvalid,
+    input signed [yi_bits-1:0]     yi,
+    input signed [yq_bits-1:0]     yq,
+    output reg                     s_axis_i_tvalid,
     output reg signed [i_bits-1:0] i,
+    output reg                     s_axis_q_tvalid,
     output reg signed [q_bits-1:0] q
     );
+
+   reg [4:0]                       pipeline;
+
+   initial begin
+      pipeline = 5'b0;
+   end
 
    /**
     * Represent each step of:
@@ -22,21 +32,62 @@ module cpx_multiply #(parameter xi_bits = 12,
     * (x + yi)(u + vi) = (xu - yv) + (xv + yu)i
     */
    reg signed [xi_bits + yi_bits:0] 	   i_sub;
+   reg signed [xi_bits + yi_bits:0]        i_sub_out;
    reg signed [xq_bits + yq_bits:0] 	   q_add;
+   reg signed [xq_bits + yq_bits:0]        q_add_out;
    reg signed [xi_bits + yi_bits:0] 	   xu;
+   reg signed [xi_bits + yi_bits:0]        xu_out;
    reg signed [xq_bits + yq_bits:0] 	   yv;
+   reg signed [xq_bits + yq_bits:0]        yv_out;
    reg signed [xi_bits + yq_bits:0] 	   xv;
+   reg signed [xi_bits + yq_bits:0]        xv_out;
    reg signed [xq_bits + yi_bits:0] 	   yu;
+   reg signed [xq_bits + yi_bits:0]        yu_out;
+   wire                                    in_valid;
+
+   assign in_valid = m_axis_x_tvalid & m_axis_y_tvalid;
 
    always @(posedge clk) begin
-      xu <= xi * yi;
-      yv <= xq * yq;
-      xv <= xi * yq;
-      yu <= xq * yi;
-      i_sub <= xu - yv;
-      q_add <= xv + yu;
-      i <= i_sub[i_bits:0];
-      q <= q_add[q_bits:0];
-   end
+      if(in_valid) begin
+         xu <= xi * yi;
+         xu_out <= xu;
+         yv <= xq * yq;
+         yv_out <= yv;
+         xv <= xi * yq;
+         xv_out <= xv;
+         yu <= xq * yi;
+         yu_out <= yu;
+         i_sub <= xu_out - yv_out;
+         i_sub_out <= i_sub;
+         q_add <= xv_out + yu_out;
+         q_add_out <= q_add;
+      end // if (in_valid)
+      else begin
+         xu <= xu;
+         xu_out <= xu_out;
+         yv <= yv;
+         yv_out <= yv_out;
+         xv <= xv;
+         xv_out <= xv_out;
+         yu <= yu;
+         yu_out <= yu_out;
+         i_sub <= i_sub;
+         i_sub_out <= i_sub_out;
+         q_add <= q_add;
+         q_add_out <= q_add_out;
+      end // else: !if(in_valid)
+      i <= i_sub_out[i_bits:0];
+      q <= q_add_out[q_bits:0];
+   end // always @ (posedge clk)
+
+   always @(posedge clk) begin
+      if(in_valid) begin
+         pipeline <= pipeline + 1'b1;
+         if (pipeline == 4'd4) begin
+            s_axis_i_tvalid <= 1'b1;
+            s_axis_q_tvalid <= 1'b1;
+         end
+      end
+   end // always @ (posedge clk)
 
 endmodule // cpx_multiply
--- src/cpx_multiply_tb.v
+++ src/cpx_multiply_tb.v
@@ -5,17 +5,23 @@ module cpx_multiply_tb();
    integer               cpx_multiply_input; // file handler
    integer               scan_file; // file handler
    integer               cpx_multiply_output;
-   
+
+   reg                   m_axis_x_tvalid;
    reg signed [{{ xi_bits - 1 }}:0] xi;
    reg signed [{{ xq_bits - 1 }}:0] xq;
+   reg                              m_axis_y_tvalid;
    reg signed [{{ yi_bits - 1 }}:0] yi;
    reg signed [{{ yq_bits - 1 }}:0] yq;
+   wire                             s_axis_i_tvalid;
    wire signed [{{ i_out_bits - 1 }}:0] i_out;
+   wire                                 s_axis_q_tvalid;
    wire signed [{{ q_out_bits - 1 }}:0] q_out;
 `define NULL 0
 
    initial begin
       clk = 1'b0;
+      m_axis_x_tvalid = 1'b0;
+      m_axis_y_tvalid = 1'b0;
       cpx_multiply_input = $fopen("{{ cpx_multiply_input }}", "r");
       if (cpx_multiply_input == `NULL) begin
          $display("cpx_multiply_input handle was NULL");
@@ -36,9 +42,14 @@ module cpx_multiply_tb();
 
    always @(posedge clk) begin
       scan_file = $fscanf(cpx_multiply_input, "%d,%d,%d,%d\n", xi,xq,yi,yq);
-      $fwrite(cpx_multiply_output, "%d,%d\n", i_out,q_out);
+      if (s_axis_i_tvalid & s_axis_q_tvalid) begin
+         $fwrite(cpx_multiply_output, "%d,%d\n", i_out,q_out);
+      end
       if ($feof(cpx_multiply_input)) begin
          $finish;
+      end else begin
+         m_axis_x_tvalid = 1'b1;
+         m_axis_y_tvalid = 1'b1;
       end
    end
 endmodule // cpx_multiply_tb
