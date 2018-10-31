f114f3acd24dcb3a6c3fa1a38eb8b03fa4137f4d
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2018-10-30 21:35:13 -0600

    Able to produce cosine as well now

--- src/sig_gen.v
+++ src/sig_gen.v
@@ -12,6 +12,7 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
     output reg                       s_axis_data_tvalid);
 
    reg [phase_bits - 1:0] phase;
+   reg [phase_bits - 1:0] phase_4;
    reg signed [n_bits -1:0] lut [0:lut_length];
    integer                  lut_iter;
    integer                  lut_filehandler;
@@ -19,6 +20,7 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
 
    initial begin
       phase = {phase_bits{1'b0}};
+      phase_4 = {phase_bits{1'b1}} / 3'd4;
       lut_filehandler = $fopen("{{ lut_filename }}", "r");
       for (lut_iter = 0; lut_iter <= lut_length; lut_iter = lut_iter + 1) begin
          scan_file = $fscanf(lut_filehandler, "%d\n", lut[lut_iter]);
@@ -28,6 +30,7 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
    always @(posedge clk) begin
       if (m_axis_data_tready) begin
          phase <= phase + freq_step;
+         phase_4 <= phase_4 + freq_step;
       end
       else begin
          phase <= phase;
@@ -37,6 +40,7 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
    always @(posedge clk) begin
       if (m_axis_data_tready) begin
          sine <= lut[phase[phase_bits - 1:phase_bits - n_bits - 1]];
+         cosine <= lut[phase_4[phase_bits - 1:phase_bits - n_bits - 1]];
          s_axis_data_tvalid <= 1'b1;
       end else begin
          s_axis_data_tvalid <= 1'b0;
