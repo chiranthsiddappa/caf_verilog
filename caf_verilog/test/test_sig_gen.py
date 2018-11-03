8646147a9af139448b325c0338970c7a696b0feb
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2018-11-03 15:04:06 -0600

    Adding tests for the phase increment and number of bits for resolution

--- caf_verilog/test/test_sig_gen.py
+++ caf_verilog/test/test_sig_gen.py
@@ -1,1 +1,19 @@
 from unittest import TestCase
+from .. import sig_gen as sg
+
+
+class TestSigGen(TestCase):
+
+    def test_phase_increment(self):
+        fclk = 120e6
+        nbits = 10
+        test_inc = sg.phase_increment(1.40625e6, nbits, fclk)
+        inc = 12
+        self.assertEqual(inc, test_inc)
+
+    def test_phase_bits(self):
+        fclk = 100e6
+        precision = 1
+        nbits = 27
+        test_nbits = sg.phase_bits(fclk, precision)
+        self.assertEqual(nbits, test_nbits)
