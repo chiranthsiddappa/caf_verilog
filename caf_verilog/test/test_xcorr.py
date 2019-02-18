e3f831943a270f0b2bbe7a3307f4a2c4a65f6043
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-02-18 14:01:55 -0700

    Verify the peak and location

--- caf_verilog/test/test_xcorr.py
+++ caf_verilog/test/test_xcorr.py
@@ -2,6 +2,8 @@ from .test_base import TestCafVerilogBase
 from gps_helper.prn import PRN
 from sk_dsp_comm import sigsys as ss
 from numpy import array
+from ..sim_helper import sim_shift
+from .. import xcorr as xc
 
 
 class TestXCorr(TestCafVerilogBase):
@@ -11,3 +13,11 @@ class TestXCorr(TestCafVerilogBase):
         prn_seq = prn.prn_seq()
         prn_seq, b = ss.NRZ_bits2(array(prn_seq), 5)
         self.prn_seq = prn_seq
+
+    def test_dot_xcorr(self):
+        center = 300
+        corr_length = 200
+        ref, rec = sim_shift(self.prn_seq, center, corr_length, shift=10)
+        rr = xc.dot_xcorr(ref, rec)
+        self.assertAlmostEqual(max(rr), 200)
+        self.assertAlmostEquals(rr[100 - 10], 200)
