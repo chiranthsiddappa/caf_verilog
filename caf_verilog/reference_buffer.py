e77122cf971204459ba81e750339f07dfde72d9f
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-03-15 16:37:54 -0600

    reference buffer write_module is writing the module with the correct buffer file

--- caf_verilog/reference_buffer.py
+++ caf_verilog/reference_buffer.py
@@ -17,7 +17,7 @@ class ReferenceBuffer(CafVerilogBase):
         self.buff_quant = quantize(self.buffer, self.i_bits, self.q_bits)
         self.reference_buffer_filename = "%s_values.txt" % (self.module_name())
         self.test_output_filename = "%s_output_values.txt" % (self.module_name())
-
+        self.module_path = os.path.join(filedir, '..', 'src', '%s.v' % (self.module_name()))
         self.write_module()
 
     def write_buffer_values(self):
@@ -33,3 +33,15 @@ class ReferenceBuffer(CafVerilogBase):
 
     def write_module(self):
         self.write_buffer_values()
+        module_template = None
+        t_dict = self.template_dict()
+        with open(self.module_path) as module_file:
+            module_template = Template(module_file.read())
+        with open(os.path.join(self.output_dir, self.module_name()+".v"), 'w+') as module_file:
+            module_file.write(module_template.render(**t_dict))
+
+    def template_dict(self):
+        t_dict = {'buffer_length': len(self.buffer), 'i_bits': self.i_bits, 'q_bits': self.q_bits}
+        rbf_path = os.path.join(self.output_dir, self.reference_buffer_filename)
+        t_dict['reference_buffer_filename'] = os.path.abspath(rbf_path)
+        return t_dict
