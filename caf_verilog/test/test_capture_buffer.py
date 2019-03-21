69b723aa3fb1e1d33e3ba87808a7140e1023c337
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-03-21 16:34:35 -0600

    Adding test case to check for all the output files from gen_tb for capture buffer

---
+++ caf_verilog/test/test_capture_buffer.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from .. import capture_buffer as capt_buff
+from tempfile import mkdtemp
+import os
+
+
+class TestCaptureBuffer(TestCase):
+
+    def test_capture_buffer(self):
+        """
+        Test that the files are written out for instantiation and testbench.
+        :return:
+        """
+        tmpdir = mkdtemp()
+        cb = capt_buff.CaptureBuffer(100, output_dir=tmpdir)
+        cb.gen_tb()
+        files = os.listdir(tmpdir)
+        test_files = ['capture_buffer.v', 'capture_buffer_tb.v', 'capture_buffer_values.txt']
+        for file in test_files:
+            self.assertIn(file, files)
