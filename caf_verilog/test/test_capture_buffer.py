d9c7124050a6ecbc28639fb0b6f8d6d1f31c5616
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-03-24 15:29:47 -0600

    Adding a test for the test file length

--- caf_verilog/test/test_capture_buffer.py
+++ caf_verilog/test/test_capture_buffer.py
@@ -18,3 +18,10 @@ class TestCaptureBuffer(TestCase):
         test_files = ['capture_buffer.v', 'capture_buffer_tb.v', 'capture_buffer_values.txt']
         for file in test_files:
             self.assertIn(file, files)
+
+    def test_capture_buffer_values_file(self):
+        tmpdir = mkdtemp()
+        cb = capt_buff.CaptureBuffer(100, output_dir=tmpdir)
+        with open(os.path.join(tmpdir, 'capture_buffer_values.txt')) as cbv:
+            lines = len(cbv.readlines())
+            self.assertEqual(100, lines)
