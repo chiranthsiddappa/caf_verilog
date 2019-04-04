807b8704219454a7799cada2cd518c969cdc1e61
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-04-04 14:42:14 -0600

    Adding simple caf function; might need a hanning window on the input from the notebook example

--- caf_verilog/caf.py
+++ caf_verilog/caf.py
@@ -20,3 +20,31 @@ class CAF(CafVerilogBase):
         self.i_bits = i_bits
         self.q_bits = q_bits
         self.output_dir = output_dir
+
+
+def simple_caf(x, y, foas, fs):
+    """
+    Produce values for a surface plot of the Complex Ambiguity Function.
+    This function primarily supports testing values produced via sim_helper, so y is expected to be twice the length of
+    x.
+    The return is the CAF surface and a time delay range normalized by the sampling frequency.
+
+    :param x: Use x as a reference signal.
+    :param y: Use y as a captured signal.
+    :param foas: Frequency offsets, provided as a list/iterable object.
+    :param fs: Sampling frequency
+    :return: caf_res, dt
+    """
+    nlags = len(x)
+    ztup = (nlags, len(foas))
+    caf_res = np.zeros(ztup)
+    nlen = len(y)
+    nrange = np.arange(0, nlen)
+    dt_lags = nlags // 2
+    dt = np.arange(-dt_lags, dt_lags) / float(fs)
+    for k, Df in enumerate(foas):
+        theta = np.exp(1j*2*np.pi*nrange*Df/float(fs))
+        y_shift = y * theta
+        rxy, lags = dc.xcorr(x, y_shift, nlags)
+        caf_res[:, k] = np.abs(rxy)
+    return caf_res, dt
