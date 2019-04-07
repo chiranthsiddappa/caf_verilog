52ea501f498602abf9f3bef9b767b9622080c104
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-04-07 13:36:56 -0600

    Adding a test for arg_max which was previously missing

---
+++ caf_verilog/test/test_arg_max.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from .. import arg_max as am
+from tempfile import mkdtemp
+import os
+
+
+class ArgMax(TestCase):
+
+    def test_arg_max_tb(self):
+        """
+        Test that the files are written out
+        :return:
+        """
+        tmpdir = mkdtemp()
+        x = [ii for ii in range(0, 100)]
+        arg_max = am.ArgMax(x, output_dir=tmpdir)
+        arg_max.gen_tb()
+        files = os.listdir(tmpdir)
+        test_files = ['arg_max_tb.v', 'arg_max.v', 'arg_max_input_values.txt']
+        for file in test_files:
+            self.assertIn(file, files)
