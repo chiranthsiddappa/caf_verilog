c46c6541a67be9139242e76eb460a0ae23bef058
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2019-04-13 16:48:24 -0600

    Ready for passing reference buffer

--- src/caf.v
+++ src/caf.v
@@ -44,10 +44,8 @@ module caf(input clk,
    reg [{{ cap_i_bits + cap_q_bits - 1 }}:0] m_axi_cap_wdata;
    wire                                      s_axi_cap_bresp;
    wire                                      s_axi_cap_bvalid;
-   wire                                    m_axi_cap_bready;
+   wire                                      m_axi_cap_bready;
 
-   assign m_axi_cap_rready = s_axis_freq_tready;
-   
    initial begin
       m_axi_cap_raddr = 'd0;
       m_axi_cap_rvalid = 1'b0;
@@ -57,7 +55,7 @@ module caf(input clk,
    end
 
 `include "caf_state_params.v"
-   
+
    reg [4:0]                               state;
 
    initial begin
@@ -68,45 +66,49 @@ module caf(input clk,
 
      genvar ithFreq;
 
-   reg [{{ ref_index_bits - 1 }}:0]        cap_start;
+   reg [{{ ref_index_bits }}:0]        cap_start;
    reg [{{ ref_index_bits - 1 }}:0]        cap_iter;
-   reg [{{ caf_foa_len - 1 }}:0]           m_axis_freq_tvalid;
+   wire [{{ caf_foa_len - 1 }}:0]          m_axis_freq_tvalid;
    reg [{{ freq_shift_phase_bits - 1 }}:0] freq_step_lut [0:{{ caf_foa_len - 1 }}];
    reg                                     neg_shift_lut [0:{{ caf_foa_len - 1 }}];
    reg [{{ freq_shift_phase_bits - 1 }}:0] freq_step [0:{{ caf_foa_len - 1 }}];
    reg                                     neg_shift [0:{{ caf_foa_len - 1 }}];
-   reg [{{ cap_i_bits - 1 }}:0]            freq_shift_xi [{{ caf_foa_len - 1 }}:0];
-   reg [{{ cap_q_bits - 1 }}:0]            freq_shift_xq [{{ caf_foa_len - 1 }}:0];
+   wire [{{ cap_i_bits - 1 }}:0]            freq_shift_xi [{{ caf_foa_len - 1 }}:0];
+   wire [{{ cap_q_bits - 1 }}:0]            freq_shift_xq [{{ caf_foa_len - 1 }}:0];
    wire [{{ caf_foa_len - 1 }}:0]          s_axis_freq_tready;
-   reg [{{ caf_foa_len - 1 }}:0]           m_axis_freq_tready;
+   wire [{{ caf_foa_len - 1 }}:0]          m_axis_freq_tready;
    wire [{{ cap_i_bits - 1 }}:0]           i_freq [{{ caf_foa_len - 1 }}:0];
    wire [{{ cap_q_bits - 1 }}:0]           q_freq [{{ caf_foa_len - 1 }}:0];
    wire [{{ caf_foa_len - 1 }}:0]          s_axis_freq_tvalid;
    reg [{{ caf_foa_len_bits - 1 }}:0]      freq_assign;
 
+   assign m_axi_cap_rready = s_axis_freq_tready;
+
    initial begin
       cap_start = 'd0;
       cap_iter = 'd0;
-      m_axis_freq_tvalid = 'd0;
       $readmemb("{{ caf_phase_increment_filename }}", freq_step_lut);
       $readmemb("{{ caf_neg_shift_filename }}", neg_shift_lut);
       freq_assign = 'd0;
    end
 
+   wire [{{ caf_foa_len - 1}}:0] s_axis_x_corr_tvalid;
+   wire [{{ out_max_bits - 1 }}:0] out_max;
+   wire [{{ length_counter_bits - 1 }}:0] x_corr_index [{{ caf_foa_len - 1 }}:0];
+   reg                                    m_axis_x_corr_tready;
+   wire [{{ caf_foa_len - 1 }}:0]         m_axis_x_corr_tvalid;
+
+   initial begin
+      m_axis_x_corr_tready = 1'b0;
+   end
+
    generate
       for (ithFreq = 0; ithFreq < {{ caf_foa_len }}; ithFreq = ithFreq + 1) begin: caf_freq
 
-         initial begin
-            m_axis_freq_tvalid[ithFreq] = 1'b0;
-            m_axis_freq_tready[ithFreq] = 1'b0;
-         end
-
-         always @(posedge clk) begin
-            m_axis_freq_tvalid[ithFreq] <= s_axi_cap_rvalid;
-            m_axis_freq_tready[ithFreq] <= 1'b1;
-            freq_shift_xi[ithFreq] <= cap_i;
-            freq_shift_xq[ithFreq] <= cap_q;
-         end
+         assign m_axis_freq_tvalid[ithFreq] = s_axi_cap_rvalid;
+         assign freq_shift_xi[ithFreq] = cap_i;
+         assign freq_shift_xq[ithFreq] = cap_q;
+         assign m_axis_x_corr_tvalid[ithFreq] = s_axis_freq_tvalid[ithFreq];
 
          {{ freq_shift_name }} #(.phase_bits({{ freq_shift_phase_bits }}),
                                  .i_bits({{ freq_shift_i_bits }}),
@@ -121,6 +123,28 @@ module caf(input clk,
                                                                                   .i(i_freq[ithFreq]),
                                                                                   .q(q_freq[ithFreq]),
                                                                                   .s_axis_tvalid(s_axis_freq_tvalid[ithFreq]));
+
+         x_corr #(.xi_bits({{ ref_i_bits }}),
+                  .xq_bits({{ ref_q_bits }}),
+                  .yi_bits({{ cap_i_bits }}),
+                  .yq_bits({{ cap_q_bits }}),
+                  .i_bits({{ sum_i_bits }}),
+                  .q_bits({{ sum_q_bits }}),
+                  .length({{ ref_buffer_length }}),
+                  .length_counter_bits({{ ref_index_bits }}),
+                  .out_max_bits({{ out_max_bits }})
+                  ) x_corr_caf (.clk(clk),
+                                .s_axis_tready(m_axis_freq_tready[ithFreq]),
+                                .xi(ref_i),
+                                .xq(ref_q),
+                                .yi(i_freq[ithFreq]),
+                                .yq(i_freq[ithFreq]),
+                                .m_axis_tready(m_axis_x_corr_tready),
+                                .m_axis_tvalid(m_axis_x_corr_tvalid[ithFreq]),
+                                .out_max(out_max[ithFreq]),
+                                .index(x_corr_index[ithFreq]),
+                                .s_axis_tvalid(s_axis_x_corr_tvalid[ithFreq])
+                                );
       end // block: caf_freq_gen
       endgenerate
 
@@ -163,28 +187,34 @@ module caf(input clk,
                cap_start <= 'd0;
                cap_iter <= 'd0;
                m_axi_cap_raddr <= 'd0;
+               m_axi_ref_raddr <= 'd0;
                m_axi_cap_rvalid <= 1'b1;
+               m_axis_x_corr_tready <= 1'b1;
             end // if (m_axi_cap_waddr == {{ cap_buffer_length - 1 }})
             else begin
                m_axi_cap_wvalid <= 1'b0;
             end // else: !if(m_axi_cap_waddr == {{ cap_buffer_length - 1 }})
           CORRELATE:
-            if(cap_start <= {{ ref_buffer_length }}) begin
-               if (cap_iter < {{ ref_buffer_length - 1}}) begin
-                  m_axi_cap_rvalid <= 1'b1;
-                  m_axi_cap_raddr <= m_axi_cap_raddr + s_axi_cap_rvalid;
-                  cap_iter <= cap_iter + s_axi_cap_rvalid;
+            begin
+               if(cap_start <= {{ ref_buffer_length }}) begin
+                  if (cap_iter < {{ ref_buffer_length - 1 }}) begin
+                     m_axi_cap_rvalid <= 1'b1;
+                     m_axi_cap_raddr <= m_axi_cap_raddr + s_axi_cap_rvalid;
+                     cap_iter <= cap_iter + s_axi_cap_rvalid;
+                  end
+                  else begin
+                     cap_iter <= 'd0;
+                     cap_start <= cap_start + 1'b1;
+                     m_axi_cap_raddr <= cap_start + 1'b1;
+                  end
                end
                else begin
-                  cap_iter <= 'd0;
-                  cap_start <= cap_start + 1'b1;
-                  m_axi_cap_raddr <= cap_start + 1'b1;
-               end
-            end
-            else begin
-               // Some logic to transition to FIND_MAX
-               m_axi_cap_rvalid <= 1'b0;
-            end
+                  // Some logic to transition to FIND_MAX
+                  state <= FIND_MAX;
+                  m_axi_cap_rvalid <= 1'b0;
+                  m_axis_x_corr_tready <= 1'b0;
+               end // else: !if(cap_start <= {{ ref_buffer_length }})
+            end // case: CORRELATE
         endcase // case (state)
      end
 
