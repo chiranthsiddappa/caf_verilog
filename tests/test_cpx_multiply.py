c54b5d1d16cbb6380623e2f4291db8226a7dfdca
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2023-11-13 11:28:13 -0700

    Use tmpdir in all cpx_multiply tests

--- tests/test_cpx_multiply.py
+++ tests/test_cpx_multiply.py
@@ -9,28 +9,33 @@ from tempfile import TemporaryDirectory
 class TestCpxMultiply(TestCase):
 
     def setUp(self):
+        self.tmpdir = TemporaryDirectory()
         n = np.arange(0, 12)
         self.x = np.cos(2 * np.pi * 0.211 * n)
         self.test_x = np.array([127., 31., -112., -85., 71., 120., -13., -126., -48., 102., 98., -55.])
 
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.tmpdir.cleanup()
+
     def test_cpx_multiply_x_real(self):
-        cpx_multiply = CpxMultiply(self.x, self.x, x_i_bits=8)
+        cpx_multiply = CpxMultiply(self.x, self.x, x_i_bits=8, output_dir=self.tmpdir.name)
         npt.assert_almost_equal(cpx_multiply.x_quant, self.test_x)
 
     def test_cpx_multiply_x_imag(self):
         xj = self.x * 1j
         x = self.x + xj
-        cpx_multiply = CpxMultiply(x, x, x_q_bits=8)
+        cpx_multiply = CpxMultiply(x, x, x_q_bits=8, output_dir=self.tmpdir.name)
         npt.assert_almost_equal(cpx_multiply.x_quant.imag, self.test_x)
 
     def test_cpx_multiply_y_real(self):
-        cpx_multiply = CpxMultiply(self.x, self.x, y_i_bits=8)
+        cpx_multiply = CpxMultiply(self.x, self.x, y_i_bits=8, output_dir=self.tmpdir.name)
         npt.assert_almost_equal(cpx_multiply.y_quant, self.test_x)
 
     def test_cpx_multiply_y_imag(self):
         xj = self.x * 1j
         x = self.x + xj
-        cpx_multiply = CpxMultiply(x, x, y_q_bits=8)
+        cpx_multiply = CpxMultiply(x, x, y_q_bits=8, output_dir=self.tmpdir.name)
         npt.assert_almost_equal(cpx_multiply.y_quant.imag, self.test_x)
 
     def test_cpx_multiply_gen_tb(self):
@@ -50,7 +55,7 @@ class TestCpxMultiply(TestCase):
         n = np.arange(0, 10)
         x = np.exp(2 * np.pi * 0.15 * n * 1j)
         y = np.exp(-2 * np.pi * 0.05 * n * 1j)
-        cpx_multiply = CpxMultiply(x, y)
+        cpx_multiply = CpxMultiply(x, y, output_dir=self.tmpdir.name)
         test_quo = np.array([ 4190209. + 0.j, 3390489. + 2462733.j, 1293993. + 3985731.j, -1293993. + 3985731.j,
                               -3390489.+2462733.j, -4190209. + 0.j, -3390489. - 2462733.j, -1293993. - 3985731.j,
                               1293993.-3985731.j, 3390489. - 2462733.j])
