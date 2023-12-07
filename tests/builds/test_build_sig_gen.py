06dad1032fb5e3bb4527a7f138affc13129c370c
Chiranth Siddappa <csiddappa@colsa.com>
2023-12-07 15:49:35 -0700

    build sig gen method name refactor

--- tests/builds/test_build_sig_gen.py
+++ tests/builds/test_build_sig_gen.py
@@ -2,22 +2,12 @@ from caf_verilog.sig_gen import SigGen
 from tempfile import TemporaryDirectory
 import os
 import unittest
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
 from caf_verilog.sim_helper import sim_get_runner
 import glob
 
 class TestSigGen(unittest.TestCase):
 
-    def setUp(self) -> None:
-        super().setUp()
-
-    def tearDown(self) -> None:
-        super().tearDown()
-
-
-    def test_sig_gen_output(self):
+    def test_build_sig_gen(self):
         with TemporaryDirectory() as tmpdir:
             sig_gen = SigGen(1200, 625e3, 8, output_dir=tmpdir)
             verilog_sources = [os.path.join(tmpdir, filename) for filename in glob.glob("%s/*.v" % tmpdir)]
