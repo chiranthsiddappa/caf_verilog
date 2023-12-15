36fef82c891e77d2fcd41b9bf41dfc38818f473a
Chiranth Siddappa <csiddappa@colsa.com>
2023-12-15 16:26:25 -0700

    Adding a params_dict to dot product pip

--- caf_verilog/dot_product.py
+++ caf_verilog/dot_product.py
@@ -58,10 +58,13 @@ class DotProduct(CafVerilogBase):
         write_quantized_output(self.output_dir, self.test_value_filename, self.x_quant, self.y_quant)
         self.write_dot_product_tb_module()
 
-    def template_dict(self, inst_name=None):
-        t_dict = {'xi_bits': self.x_i_bits, 'xq_bits': self.x_q_bits, 'yi_bits': self.y_i_bits,
+    def params_dict(self) -> dict:
+        return {'xi_bits': self.x_i_bits, 'xq_bits': self.x_q_bits, 'yi_bits': self.y_i_bits,
                   'yq_bits': self.y_q_bits, 'length': self.length, 'sum_i_bits': self.sum_i_bits,
                   'sum_q_bits': self.sum_q_bits}
+
+    def template_dict(self, inst_name=None):
+        t_dict = self.params_dict()
         t_dict['dot_prod_input'] = os.path.abspath(os.path.join(self.output_dir, self.test_value_filename))
         t_dict['dot_prod_output'] = os.path.abspath(os.path.join(self.output_dir, self.test_output_filename))
         t_dict['dot_prod_name'] = inst_name if inst_name else '%s_tb' % self.module_name()
