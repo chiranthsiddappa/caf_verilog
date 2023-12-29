5ad65bd87d3435f4273caa01eb58b29cfb73a9b0
Chiranth Siddappa <csiddappa@colsa.com>
2023-12-28 18:50:14 -0700

    Tests for freq shift by fft argmax

--- tests/sim/freq_shift/test_freq_shift.py
+++ tests/sim/freq_shift/test_freq_shift.py
@@ -1,3 +1,4 @@
+from caf_verilog.sig_gen import calc_smallest_phase_size, phase_increment
 from caf_verilog.freq_shift import FreqShift, send_test_input_data, capture_test_output_data
 from caf_verilog.quantizer import quantize
 from caf_verilog.sim_helper import sim_get_runner
@@ -11,12 +12,19 @@ from tempfile import TemporaryDirectory
 
 import numpy as np
 from numpy import testing as npt
+from sk_dsp_comm.digitalcom import my_psd
 
 fs = 625e3
 freq_res = 200
+f_out = 50e3
+f_shift = 20e3
 n_bits = 8
 n = np.arange(0,10e3)
-x = np.exp(2*np.pi*((50e3)/fs)*n*1j)
+x = np.exp(2*np.pi*((f_out)/fs)*n*1j)
+x_q = quantize(x, n_bits)
+x_shift_gen = np.exp(2*np.pi*((f_shift)/fs)*n*1j)
+x_shift_gen_q = quantize(x_shift_gen, n_bits)
+x_shifted_exp_q = x_q * x_shift_gen_q
 
 @cocotb.test()
 async def verify_freq_shift(dut):
@@ -26,6 +34,10 @@ async def verify_freq_shift(dut):
     x_quant = quantize(x, 12)
     x_val_iter = 0
 
+    # Step related calcs
+    num_phase_bits = calc_smallest_phase_size(fs, freq_res, n_bits)
+    increment = phase_increment(f_out=f_shift, phase_bits=num_phase_bits, f_clk=fs)
+
     cocotb.start_soon(clock.start(start_high=False))
     dut.m_axis_tready.value = 0
     dut.m_axis_tvalid.value = 0
@@ -34,6 +46,7 @@ async def verify_freq_shift(dut):
     await RisingEdge(dut.clk)
     assert dut.s_axis_tvalid == 0
     assert dut.s_axis_tready == 0
+    dut.freq_step.value = increment
 
     while (dut.s_axis_tready.value == 0):
         await RisingEdge(dut.clk)
@@ -62,6 +75,16 @@ async def verify_freq_shift(dut):
 
     assert len(output_cap) == vals
     npt.assert_equal(output_cap[0].real, 1015)
+
+    # Verify signal frequencies after shift
+    # Cosine
+    Px_gen_cos, f_gen = my_psd(x_shifted_exp_q.real, 2**12, fs)
+    Px_cos_q, f_q = my_psd(np.array(output_cap).real, 2**12, fs)
+    assert np.argmax(Px_gen_cos) == np.argmax(Px_cos_q)
+    # Sine
+    Px_gen_sin, f_gen = my_psd(x_shifted_exp_q.imag, 2**12, fs)
+    Px_sin_q, f_q = my_psd(np.array(output_cap).imag, 2**12, fs)
+    assert np.argmax(Px_sin_q) == np.argmax(Px_gen_sin)
 
 
 def test_via_cocotb():
