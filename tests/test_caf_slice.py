2d90064ad39cd03b7d11005d112ad92036a19b8a
Chiranth Siddappa <csiddappa@colsa.com>
2024-02-13 11:12:08 -0700

    Instantiate CAF Slice with known freq_res and fs

--- tests/test_caf_slice.py
+++ tests/test_caf_slice.py
@@ -9,12 +9,14 @@ class TestCAFSlice(TestCase):
 
     def test_caf_slice_output_files(self):
         test_files = ['caf_slice.v', 'cpx_multiply.v', 'dot_prod_pip.v',
-                      'freq_shift_625_23_8.v', 'sig_gen_625_23_8.v', 'x_corr.v']
+                      'freq_shift_625_12_8.v', 'sig_gen_625_12_8.v', 'x_corr.v']
         x = [ii for ii in range(0, 10)]
         y = [ii for ii in range(0, 20)]
-        foas = arange(-10, 10) * 0.25
+        fs = 625e3
+        freq_res = 200
+        n_bits = 8
         with TemporaryDirectory() as tmpdir:
-            caf_slice_inst = caf_slice.CAFSlice(x, y, foas, output_dir=tmpdir)
+            caf_slice_inst = caf_slice.CAFSlice(x, y, freq_res=freq_res, fs=fs, n_bits=n_bits, output_dir=tmpdir)
             files = os.listdir(tmpdir)
             for file in test_files:
                 self.assertIn(file, files)
