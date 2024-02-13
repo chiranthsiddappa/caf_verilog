8a7aa77628e37dd2d345a995392c456f30411a6a
Chiranth Siddappa <csiddappa@colsa.com>
2024-02-13 10:38:39 -0700

    Apply frequency shift to simulated received signal.

--- caf_verilog/sim_helper.py
+++ caf_verilog/sim_helper.py
@@ -11,7 +11,7 @@ def sim_shift(ref, ref_center, ref_length, shift=0, rec=None, padding=False,
     :param shift: How much shift should be added to the simulated received signal.
     :param rec: A received signal can be provided for the correlation simulation.
     :param padding: Use padding to add zeros on the reference signal; ex. for generating a plot.
-    :param freq_shift: Apply a frequency shift to the reference signal
+    :param freq_shift: Apply a frequency shift to the reference signal, and return with shift as reference.
     :param fs: Sampling Frequency
     :return: ref, rec
     :rtype: tuple
@@ -29,7 +29,7 @@ def sim_shift(ref, ref_center, ref_length, shift=0, rec=None, padding=False,
     t = np.arange(len(ref))
     x_shift = np.exp(2 * np.pi * (freq_shift / fs) * t * 1j)
     ref_plus_shift = [ref[i] * x_i_shift for i, x_i_shift in enumerate(x_shift)]
-    ref_ret = ref_plus_shift[ref_center - fill_length: ref_center + fill_length]
+    ref_ret = ref[ref_center - fill_length: ref_center + fill_length]
     if padding:
         fill_zeros = [0 for zz in range(0, fill_length)]
         ref_ret = fill_zeros + list(ref_ret)
@@ -37,5 +37,5 @@ def sim_shift(ref, ref_center, ref_length, shift=0, rec=None, padding=False,
     if rec:
         rec_ret = rec[sim_center - ref_length:sim_center + ref_length]
     else:
-        rec_ret = ref[sim_center - ref_length: sim_center + ref_length]
+        rec_ret = ref_plus_shift[sim_center - ref_length: sim_center + ref_length]
     return ref_ret, rec_ret
