9dc6e65b020c742a4fcb39ce370d72d5800b93e6
Chiranth Siddappa <csiddappa@colsa.com>
2024-02-26 21:15:01 -0700

    Sig Gen pipeline and tests updated

--- src/sig_gen.v
+++ src/sig_gen.v
@@ -17,6 +17,13 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
    reg signed [n_bits -1:0] lut [0:lut_length];
    reg [phase_bits - 1:0]   freq_step_buff;
    reg [1:0]                freq_step_set;
+   wire                     phase_increment_m_axis_condition;
+   wire                     phase_increment_step_condition;
+   wire                     s_axis_valid_condition;
+
+   assign phase_increment_m_axis_condition = (m_axis_data_tready & (freq_step_set[1] | freq_step_set[0]));
+   assign phase_increment_step_condition = freq_step_set[0] & ~freq_step_set[1];
+   assign s_axis_valid_condition = (freq_step_set[0] & ~freq_step_set[1]) | (m_axis_data_tready & freq_step_set[1]);
 
    initial begin
       s_axis_data_tvalid = 1'b0;
@@ -29,16 +36,28 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
 
    always @(posedge clk) begin
       if (m_axis_freq_step_tvalid) begin
-         if (freq_step_buff != freq_step) begin
             freq_step_buff <= freq_step;
             freq_step_set[0] <= 1'b1;
-         end
+      end else begin
+         freq_step_set[0] <= freq_step_set[0];
       end
-      freq_step_set[1] <= freq_step_set[0];
    end
 
    always @(posedge clk) begin
-      if (m_axis_data_tready & (freq_step_set[1] | freq_step_set[0])) begin
+      if (freq_step_set[1] & m_axis_freq_step_tvalid) begin
+         if (freq_step_buff != freq_step) begin
+            freq_step_set[1] <= 1'b0;
+         end else begin
+            freq_step_set[1] <= freq_step_set[0];
+         end
+      end
+      else begin
+         freq_step_set[1] <= freq_step_set[0];
+      end
+   end
+
+   always @(posedge clk) begin
+      if (phase_increment_step_condition | phase_increment_m_axis_condition) begin
          phase <= phase + freq_step_buff;
          phase_4 <= phase_4 + freq_step_buff;
       end
@@ -49,15 +68,21 @@ module {{ sig_gen_name }} #(parameter phase_bits = 32,
    end // always @ (posedge clk)
 
    always @(posedge clk) begin
-      if (freq_step_set[1]) begin
+      if (s_axis_valid_condition) begin
          sine <= lut[phase[phase_bits - 1:phase_bits - n_bits - 1]];
          cosine <= lut[phase_4[phase_bits - 1:phase_bits - n_bits - 1]];
          s_axis_data_tvalid <= 1'b1;
-      end else begin
+      end
+      else begin
          sine <= sine;
          cosine <= cosine;
-         s_axis_data_tvalid <= 1'b0;
+         s_axis_data_tvalid <= freq_step_set[1];
       end
+   end // always @ (posedge clk)
+
+   initial begin
+      $dumpfile("{{ sig_gen_name }}.vcd");
+      $dumpvars(2, {{ sig_gen_name }});
    end
 
 endmodule // sig_gen
