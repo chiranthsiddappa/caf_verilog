b768a1c6e7455afb198e1f8319a9f170e4ab5111
Chiranth Siddappa <csiddappa@colsa.com>
2024-03-15 16:41:02 -0600

    foas counter transactions verified in caf

--- src/caf.v
+++ src/caf.v
@@ -14,39 +14,46 @@ module caf #(parameter phase_bits = 10,
              parameter out_max_bits = 64
              )
    (input clk,
-    input [phase_bits - 1:0]             freq_step,
-    input                                freq_step_valid,
-    output reg                           freq_step_tready,
-    output reg [foas_counter_bits - 1:0] freq_step_index,
-    input                                neg_shift,
-    input                                m_axis_tvalid,
-    input [xi_bits - 1:0]                xi,
-    input [xq_bits - 1:0]                xq,
-    input [yi_bits - 1:0]                yi,
-    input [yq_bits - 1:0]                yq,
-    output                               s_axis_tready,
-    input                                m_axis_tready,
-    output [out_max_bits -1:0]           out_max,
-    output [length_counter_bits - 1:0]   index,
-    output reg                           s_axis_tvalid);
+    input                              s_axis_freq_step_tready,
+    input [phase_bits - 1:0]           freq_step,
+    input                              s_axis_freq_step_valid,
+    output reg                         m_axis_freq_step_tready,
+    output [foas_counter_bits - 1:0]   freq_step_index,
+    output reg                         freq_step_index_tvalid,
+    input                              neg_shift,
+    input                              m_axis_tvalid,
+    input [xi_bits - 1:0]              xi,
+    input [xq_bits - 1:0]              xq,
+    input [yi_bits - 1:0]              yi,
+    input [yq_bits - 1:0]              yq,
+    output                             s_axis_tready,
+    input                              m_axis_tready,
+    output [out_max_bits -1:0]         out_max,
+    output [length_counter_bits - 1:0] index,
+    output reg                         s_axis_tvalid);
 
-   parameter                             INCREMENT_INIT = 3'b000;
-   parameter                             IDLE = 3'b001;
-   parameter                             CORRELATE = 3'b010;
-   parameter                             FIND_MAX = 3'b011;
-   parameter                             RETURN_MAX = 3'b100;
+   parameter                           INCREMENT_INIT = 3'b000;
+   parameter                           IDLE = 3'b001;
+   parameter                           CORRELATE = 3'b010;
+   parameter                           FIND_MAX = 3'b011;
+   parameter                           RETURN_MAX = 3'b100;
 
-   reg                                   all_freq_steps_set;
-   reg [2:0]                             state;
-   reg signed [length_counter_bits:0]    foas_index_counter;
-   reg [foas-1:0]                        foas_index_valid;
-   reg [phase_bits - 1:0]                freq_step_capture_buff;
-   wire [31:0]                           foas_index_counter_extended;
+   reg                                 all_freq_steps_set;
+   reg [2:0]                           state;
 
-   assign foas_index_counter_extended = { {(31 - foas_counter_bits){foas_index_counter[foas_counter_bits]}}, foas_index_counter};
+   // FOAS
+   reg                                 foas_transaction_started;
+   reg [foas_counter_bits:0]           foas_index_counter;
+   reg [foas-1:0]                      foas_index_valid;
+   reg [foas-1:0]                      neg_shift_index;
+   reg [phase_bits - 1:0]              freq_step_capture_buff;
+   wire [31:0]                         foas_index_counter_extended;
+
+   assign freq_step_index = foas_index_counter[foas_counter_bits - 1:0];
+   assign foas_index_counter_extended = { {(31 - foas_counter_bits){1'b0}}, foas_index_counter};
 
    initial begin
-      freq_step_tready = 1'b0;
+      m_axis_freq_step_tready = 1'b0;
       freq_step_index = 'd0;
       s_axis_tvalid = 1'b0;
       foas_index_counter = 'd0;
@@ -56,7 +63,7 @@ module caf #(parameter phase_bits = 10,
    always @(posedge clk) begin
       case (state)
         INCREMENT_INIT: begin
-           if ((foas_index_counter_extended == foas) && (freq_step_valid == 1'b1)) begin
+           if ((foas_index_counter_extended == (foas - 1)) && (s_axis_freq_step_valid == 1'b1)) begin
               state <= CORRELATE;
               all_freq_steps_set <= 1'b1;
            end else begin
@@ -76,15 +83,34 @@ module caf #(parameter phase_bits = 10,
 
    always @(posedge clk) begin
       if (state == INCREMENT_INIT) begin
-         freq_step_tready <= 1'b1;
-         if (freq_step_valid) begin
-            freq_step_index <= freq_step_index + 1'b1;
-            foas_index_valid <= 1'b1 << freq_step_index;
+         // Output master logic
+         if ((foas_index_counter_extended == (foas - 1)) && (s_axis_freq_step_valid == 1'b1)) begin
+            m_axis_freq_step_tready <= 1'b0;
+            foas_index_counter <= 'd0;
+         end
+         else if (foas_index_counter_extended < (foas - 1)) begin
+            m_axis_freq_step_tready <= 1'b1;
+            if (s_axis_freq_step_tready) begin
+               foas_index_counter <= foas_index_counter + 1'b1;
+            end
+         end
+         // Internal logic
+         if (s_axis_freq_step_valid) begin
             freq_step_capture_buff <= freq_step;
+            foas_index_valid <= 1'b1 << foas_index_counter;
+            if (neg_shift) begin
+               neg_shift_index <= 1'b1 << foas_index_counter;
+            end
+            else begin
+               neg_shift_index <= 'd0;
+            end
+         end else begin
+            freq_step_capture_buff <= freq_step_capture_buff;
+            foas_index_valid <= foas_index_valid;
          end
       end else begin
-         freq_step_tready <= 1'b0;
-         freq_step_index <= 'd0;
+         m_axis_freq_step_tready <= 1'b0;
+         foas_index_counter <= 'd0;
          foas_index_valid <= 'd0;
          freq_step_capture_buff <= freq_step_capture_buff;
       end // else: !if(state == INCREMENT_INIT)
